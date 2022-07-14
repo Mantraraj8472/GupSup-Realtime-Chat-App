59888cd3856b0044c17d5767e9997eafafec482e
Mantra3265 <mantrarajgotecha@gmail.com>
2022-07-14 21:06:15 +0530

    Removed errors

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:gup_sup/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const GupSup());
+    await tester.pumpWidget(GupSup());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
