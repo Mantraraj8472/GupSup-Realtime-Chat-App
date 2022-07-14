2abe16e8bcafcd2d77797947a1d35f7752cec793
Mantra3265 <mantrarajgotecha@gmail.com>
2022-07-14 21:01:27 +0530

    Created display picture file for displaying picture taken from camera as well as gallery

--- lib/Screens/displayPicture.dart
+++ lib/Screens/displayPicture.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'cameraScreen.dart';
 
@@ -75,88 +74,80 @@ class DisplayPicture extends StatelessWidget {
                       ),
                     ],
                   ),
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: 90,
-                        color: Color(0xffF5F5F5),
+                ],
+              ),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 90,
+              color: Color(0xffF5F5F5),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 8.0, bottom: 4),
+                      child: CircleAvatar(
+                        radius: 17,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Color(0xff4E5152),
+                        ),
+                        backgroundColor: Color(0xffE2E2E2),
+                      ),
+                    ),
+                    Expanded(
+                      child: Card(
+                        margin: const EdgeInsets.only(
+                          top: 8,
+                          left: 2,
+                          right: 2,
+                          bottom: 22,
+                        ),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                        color: const Color(0xffF5F5F5),
                         child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Row(
-                            children: [
-                              Padding(
-                                padding: const EdgeInsets.only(
-                                    right: 8.0, bottom: 4),
-                                child: CircleAvatar(
-                                  radius: 17,
-                                  child: Icon(
-                                    Icons.camera_alt,
-                                    color: Color(0xff4E5152),
-                                  ),
-                                  backgroundColor: Color(0xffE2E2E2),
-                                ),
-                              ),
-                              Expanded(
-                                child: Card(
-                                  margin: const EdgeInsets.only(
-                                    top: 8,
-                                    left: 2,
-                                    right: 2,
-                                    bottom: 22,
-                                  ),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(25),
-                                  ),
-                                  color: const Color(0xffF5F5F5),
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 15.0),
-                                    child: TextFormField(
-                                      textAlignVertical:
-                                          TextAlignVertical.center,
-                                      keyboardType: TextInputType.multiline,
-                                      minLines: 1,
-                                      maxLines: 5,
-                                      style: const TextStyle(
-                                        fontSize: 20,
-                                      ),
-                                      decoration: InputDecoration(
-                                        hintText: 'Add a caption...',
-                                        contentPadding:
-                                            EdgeInsets.only(bottom: 6),
-                                        border: InputBorder.none,
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              Padding(
-                                padding: EdgeInsets.only(
-                                  top: 8,
-                                  left: 4,
-                                  right: 2,
-                                  bottom: 20,
-                                ),
-                                child: CircleAvatar(
-                                  radius: 20,
-                                  child: Padding(
-                                    padding: const EdgeInsets.only(left: 4.0),
-                                    child: Icon(
-                                      Icons.send,
-                                      color: Colors.white,
-                                    ),
-                                  ),
-                                  backgroundColor: Color(0xffE76F52),
-                                ),
-                              ),
-                            ],
+                          padding: const EdgeInsets.only(left: 15.0),
+                          child: TextFormField(
+                            textAlignVertical: TextAlignVertical.center,
+                            keyboardType: TextInputType.multiline,
+                            minLines: 1,
+                            maxLines: 5,
+                            style: const TextStyle(
+                              fontSize: 20,
+                            ),
+                            decoration: InputDecoration(
+                              hintText: 'Add a caption...',
+                              contentPadding: EdgeInsets.only(bottom: 6),
+                              border: InputBorder.none,
+                            ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ],
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: 8,
+                        left: 4,
+                        right: 2,
+                        bottom: 20,
+                      ),
+                      child: CircleAvatar(
+                        radius: 20,
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 4.0),
+                          child: Icon(
+                            Icons.send,
+                            color: Colors.white,
+                          ),
+                        ),
+                        backgroundColor: Color(0xffE76F52),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
